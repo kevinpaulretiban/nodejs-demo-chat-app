ac9d1d0fef09f1cf5c16cb1460cfa05a3d71eef1
Kevin Paul Retiban <kretiban@yondu.com>
2019-07-18 15:09:54 +0800

    Connect to MongoDB using Mongoose

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ var bodyParser = require('body-parser')
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+var mongoose = require('mongoose')
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+
+var dbUrl = 'mongodb://user:user@ds155424.mlab.com:55424/learning-node'
 
 var messages = [
     { name: 'Kevin', message: 'Hi' },
@@ -25,6 +28,10 @@ app.post('/messages', (req, res) => {
 
 io.on('connection', (socket) => {
     console.log('a user connected')
+})
+
+mongoose.connect(dbUrl, { useMongoClient: true }, (err) => {
+    console.log('mongo db connection', err)
 })
 
 var server = http.listen(3000, () => {
